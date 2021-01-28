7820b861bdb869f1e37a7fb005e8adcc0222f648
otakuprograma2020 <jaootaku@outlook.com>
2021-01-28 00:29:53 -0300

    update user-agent

--- manga-anime/Common.py
+++ manga-anime/Common.py
@@ -11,7 +11,7 @@ class Common:
         else:
             self.feature = 'html.parser'
         self.headers = {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
         }
         self.chrome_options = chromeOptions.Options()
         if saida_path:
